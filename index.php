eb64701a22623cfb8fd0bd391146441a92463289
elfesyaesen <web.elfesyaesen@gmail.com>
2024-11-07 01:37:04 +0300

    update commit

--- index.php
+++ index.php
@@ -1,3 +1,80 @@
 <?php
 require_once __DIR__ . '/config.php';
-print 'TEST';
+
+// print_r($array['products']['işlemci tipi'][0]);
+// foreach , for , while 
+
+// foreach ($array as $key => $value) {
+//     if ($key !== 'product') {
+//         print 'Anahtar : ' . $key . ' , Değer : ' . $value . '<hr/>';
+//     }
+// }
+
+// print count($array);
+// exit;
+
+// for ($i = 0; $i <= count($array); $i++) {
+//     print $array[$i] . '<hr/>';
+// }
+
+
+
+
+// $a = 0;
+
+// while ($a <= 5) {
+//     print $array[$a] . '<hr/>';
+
+//     $a++;
+// }
+
+
+
+// print ($a > $b) ? 'Büyüktür.' : 'Değildir.';
+
+// $a = "";
+// $b = 20;
+// null kontrol
+// print $a ?? 'değersiz.';
+
+// dolu boş kontrol
+// print $a ?: 'değersiz.';
+
+// $new_array = array_map(function ($item) {
+//     return 'Kırtasiye : ' . $item;
+// }, $array);
+
+
+// $array =
+//     [
+//         "Bilgisayar",
+//         "Kitap",
+//         "Defter",
+//         "Kalem",
+//         "Silgi",
+//         "Çanta",
+//     ];
+
+// $new_array = array_map(function ($item) {
+//     return 'Kırtasiye : ' . $item;
+// }, $array);
+
+// $string = "Bilgisayar,Kitap,Defter,Kalem,Silgi,Çanta";
+
+// $array = explode(',', $string);
+
+// $array =
+//     [
+//         "Bilgisayar",
+//         "Kitap",
+//         "Defter",
+//         "Kalem",
+//         "Silgi",
+//         "Çanta",
+//     ];
+
+// unset($array[2]);
+
+// $string = implode('<=>', $array);
+
+// print_r($array);
