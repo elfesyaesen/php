c361ef3f8a21b6e85c41af94e159f3bd7fcfd9dc
elfesyaesen <web.elfesyaesen@gmail.com>
2024-11-07 01:35:09 +0300

    update

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+define('APP_URL', 'http://127.0.0.1:8000/');
+define('APP_ROOT', __DIR__);
--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-
-$variable = 'Kasım YÜKSEL & Eren CENNETKUŞU';
-$args1    = 20;
-$args2    = '20';
-
-if ($args1 === $args2) {
-    print 'eşittir.';
-} else if ($args1 > $args2) {
-    print $args1 . ' : büyüktür.';
-} else if ($args1 < $args2) {
-    print $args1 . ' Küçüktür ' . $args;
-} else {
-    print 'koşul dışı...';
-}
+require_once __DIR__ . '/config.php';
+print 'TEST';
